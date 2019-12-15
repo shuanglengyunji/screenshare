adb97dfffda4e66bbef4d33a3f91cd766f00c665
shuanglengyunji <liuhan211211@gmail.com>
2019-12-15 14:13:00 +1100

    Add cursor

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template, Response
 from camera import VideoCamera
-# import pyscreenshot
 import flask
 from PIL import ImageGrab
+from PIL import Image
 from io import BytesIO
 import socket
+import win32gui
 import logging
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
+
+imCursor = Image.open('cursor.png')
 
 app = Flask(__name__)
 
@@ -16,28 +19,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-#--------------------------------------------------------------------------------
-
-# feed from camera
-# gen: convert img byte to frame
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-# video_feed feed img to internet
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-#--------------------------------------------------------------------------------
-
+# Grab & Feed Screen Shot
 @app.route('/screen.png')
 def serve_pil_image():
+    # Grab Screen 
+    imScreen=ImageGrab.grab()
+    # Get Cursor
+    curX,curY=win32gui.GetCursorPos()
+    # Add Cursor
+    imScreen.paste(imCursor,box=(curX,curY),mask=imCursor)
+    # Save to BytesIO
     img_buffer = BytesIO()
-    ImageGrab.grab().save(img_buffer, 'PNG', quality=10)
+    imScreen.save(img_buffer,'PNG',quality=10)
     img_buffer.seek(0)
     return flask.send_file(img_buffer, mimetype='image/png')
 
@@ -53,6 +57,8 @@ if __name__ == '__main__':
 	app.run('0.0.0.0', debug=False)
 
 '''
+ImageGrab.grab().save(img_buffer, 'PNG', quality=10)
+
 # app.run(host=get_ip(), debug=False)
 def get_ip():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
